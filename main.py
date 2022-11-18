b971cbd305c91e7772c3d6ef49ff033001ef1bc0
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-18 12:37:40 +1100

    🎉 Update

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import pygame
+import curses
+
+
+def main():
+    s = curses.initscr()
+    curses.curs_set(0)
+
+    sh, sw = s.getmaxyx()
+    window = curses.newwin(sh, sw, 0, 0)
+
+
+if __name__ == "__main__":
+    main()
