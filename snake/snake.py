d7af26ef99133f75b719ae94303f91453521a23a
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-18 21:50:37 +1100

    🎉 Update

--- snake/snake.py
+++ snake/snake.py
@@ -6,7 +6,6 @@ class Snake:
         self.width = width
         self.height = height
 
-        # **** Could be optimized by using a LinkedList (the head is the -1 element)
         self.snake = [[self.height // 2, self.width // 2]]
         self.food = self.select_food()
 
@@ -14,31 +13,28 @@ class Snake:
 
         self.terminated = False
 
+        self.available = set()
+        for i in range(height):
+            for j in range(width):
+                if (i != self.snake[0][0] or j != self.snake[0][1]) and (i != self.food[0] or j != self.food[1]):
+                    self.available.add((i, j))
+
     # Get the dimensions of the game
     def get_dims(self):
         return self.height, self.width
 
     # Select a random location for food
-    # ***** This can be better optimized using a reverse mapping
     def select_food(self):
-        food = None
-
-        while True:
-            food = [
-                random.randint(0, self.height - 1),
-                random.randint(0, self.width - 1)
-            ]
-            if food not in self.snake:
-                return food
+        return random.choice(self.available)
 
     # Check if the game is over
     def game_over(self):
-        return self.terminated
+        return self.terminated or len(self.available) == 0
 
     # Update the state of the game
     def update_state(self, key=None):
         if self.game_over():
-            raise Exception("Game has already finished")
+            raise Exception("Game has finished")
 
         # Check for collision / out of bounds
         if self.snake[0] in self.snake[1:] or self.snake[0][0] in [-1, self.height] or self.snake[0][1] in [-1, self.width]:
@@ -61,7 +57,10 @@ class Snake:
             elif key == 3:
                 self.dir = [0, 1]  # Right
 
-        self.snake.insert(
-            0, (self.snake[0] + self.dir[0], self.snake[1] + self.dir[1])
-        )
-        self.snake.pop(-1)
+        new_pos = (self.snake[0] + self.dir[0], self.snake[1] + self.dir[1])
+        self.snake.insert(0, new_pos)
+        tail = self.snake.pop(-1)
+
+        if new_pos in self.available:
+            self.available.remove(new_pos)
+        self.available.add(tail)
