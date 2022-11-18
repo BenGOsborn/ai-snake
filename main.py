3e79d7a785e5323aa4baeef1d9d4e5f5a3ab6838
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-18 20:57:53 +1100

    🎉 Update

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import curses
 
 
-def main():
+def main(stdscr):
     s = curses.initscr()
-    curses.curs_set(0)
 
-    sh, sw = s.getmaxyx()
-    window = curses.newwin(sh, sw, 0, 0)
+    sh, sw = stdscr.getmaxyx()
+
+    stdscr.clear()
+
+    stdscr.addch(sh // 2, sw // 2, curses.ACS_PI)
+    stdscr.addch(sh // 2, sw // 2 + 1, curses.ACS_BLOCK)
+
+    stdscr.refresh()
+
+    stdscr.getch()
 
 
 if __name__ == "__main__":
-    main()
+    curses.wrapper(main)
--- snake/snake.py
+++ snake/snake.py
@@ -1,2 +1,35 @@
+import random
+
+
 class Snake:
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.snake = [[self.width // 2, self.height // 2]]
+        self.food = self.select_food()
+
+        self.dir = [1, 0]
+
+        self.terminated = False
+
+    # Get the dimensions of the game
+    def get_dims(self):
+        return self.width, self.height
+
+    # Select a random location for food
+    def select_food(self):
+        return random.randint(0, self.height - 1), random.randint(0, self.width - 1)
+
+    # Check if the game is over
+    def game_over(self):
+        return self.terminated
+
+    # Update the state of the game
+    def update_state(self, key=None):
+        # **** Check for food
+        # **** Check for collision / out of bounds
+        # **** Update position of snake
+        # **** Terminate game if necessary
+
+        pass
