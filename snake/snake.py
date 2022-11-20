10b1dce9dc94ab2975d55e47c39b5f1713335db4
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-20 20:29:34 +1100

    🎉 Update

--- snake/snake.py
+++ snake/snake.py
@@ -18,6 +18,7 @@ class Snake:
     # Reset the state of the game
     def reset(self):
         self.random = random.Random(1234)
+        # self.random = random.Random()
         self.snake = [
             (
                 self.random.randint(0, self.height - 1),
