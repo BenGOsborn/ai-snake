e8fa1e8e002f3df84d0e8d7140163390dd80d33d
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-21 08:21:19 +1100

    🎉 Update

--- model/trainer.py
+++ model/trainer.py
@@ -25,32 +25,22 @@ class Trainer:
         for agent in self.generation:
             agent.evaluate()
 
-    # Breed two agents together
-    def breed(self, agent1, agent2):
-        state1 = agent1.model.state_dict()
-        state2 = agent2.model.state_dict()
+    # Mutate an agent
+    def mutate(self, agent):
+        state = agent.model.state_dict()
 
         # Merge genes together randomly
         new_genes = {}
 
         # Mutate all genes
-        mutation_bound = [
-            -self.mutation_chance / 2,
-            self.mutation_chance / 2
-        ]
+        for key in state:
+            rand1 = 2 * torch.rand(state[key].shape) - 1
+            rand2 = 2 * torch.rand(state[key].shape) - 1
 
-        for key in state1:
-            rand1 = 2 * torch.rand(state1[key].shape) - 1
+            original = (rand1 >= self.mutation_chance) * state[key]
+            mutated = (rand1 < self.mutation_chance) * rand2
 
-            genes_state1 = (rand1 <= mutation_bound[0]) * state1[key]
-            genes_state2 = (rand1 > mutation_bound[1]) * state2[key]
-
-            rand2 = 2 * torch.rand(state1[key].shape) - 1
-
-            genes_mutation = ((rand1 > mutation_bound[0]) & (
-                rand1 <= mutation_bound[1])) * rand2
-
-            genes = genes_state1 + genes_state2 + genes_mutation
+            genes = original + mutated
             new_genes[key] = genes
 
         # Create new child with new genes
@@ -80,22 +70,20 @@ class Trainer:
             self.best_fitness = fitness[argmax]
             self.best_agent = self.generation[argmax]
 
-        # Breed fit agents to create new generation
+        # Mutate agents to create better ones
         probs = torch.softmax(fitness, dim=0)
         distribution = torch.distributions.categorical.Categorical(probs=probs)
 
         new_generation = []
 
         while len(new_generation) < int(len(self.generation) * (1 - self.mutation_chance)):
-            parent1, parent2 = distribution.sample((2,)).tolist()
+            # Add original genes
+            original = distribution.sample().item()
+            new_generation.append(self.generation[original])
 
-            child = self.breed(
-                self.generation[parent1],
-                self.generation[parent2]
-            )
-
-            new_generation.append(child)  # Add the new child
-            new_generation.append(self.generation[parent1])  # Add the parent
+            # Add mutated genes
+            mutated = self.mutate(self.generation[original])
+            new_generation.append(mutated)
 
         for _ in range(len(self.generation) - len(new_generation)):
             # Add random genes to the pool
