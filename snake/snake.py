3f21ab52fba965de03c2508830e0242b70018445
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-20 20:56:15 +1100

    🎉 Update

--- snake/snake.py
+++ snake/snake.py
@@ -1,7 +1,5 @@
 import random
 
-# **** We should update the snake so that it actually respawns immediately - the food stays in the same position from its inception
-
 
 class Snake:
     def __init__(self, height, width, seed=None):
