bbc5f12eeb8b643426e50d4fad17ddb4490e3d26
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-20 20:13:19 +1100

    🎉 Update

--- model/agent.py
+++ model/agent.py
@@ -10,60 +10,48 @@ class Agent:
 
         self.fitness = None
 
-    # Evaluate the current agent a given amount of times
-    def evaluate(self, evaluations=1, time_limit=1000, stuck_limit=100):
-        fitness = []
-        deaths = 0
-        steps = []
-        penalties = 0
+    # Evaluate the current agent
+    def evaluate(self, time_limit=1000, stuck_limit=100):
+        time = 0
 
-        for _ in range(evaluations):
-            time = 0
+        eating_times = [0]  # The times the snake ate
+        prev_size = len(self.snake.snake)
+        penalty = 0
 
-            eating_times = [0]  # The times the snake ate
-            prev_size = len(self.snake.snake)
-            penalty = 0
+        # Run the game loop
+        while not self.snake.game_over() and time < time_limit:
+            # Check if the snake is stuck
+            if time - eating_times[-1] == stuck_limit:
+                penalty = 1
+                break
 
-            # Run the game loop
-            while not self.snake.game_over() and time < time_limit:
-                # Check if the snake is stuck
-                if time - eating_times[-1] == stuck_limit:
-                    penalty = 1
-                    break
+            # Choose a key and update the state
+            key = utils.choose_key(
+                self.snake.get_game_state(),
+                self.model
+            )
+            self.snake.update_state(key)
 
-                # Choose a key and update the state
-                key = utils.choose_key(
-                    self.snake.get_game_state(),
-                    self.model
-                )
-                self.snake.update_state(key)
+            # Record the eating times of the snake
+            current_size = len(self.snake.snake)
+            if current_size > prev_size:
+                eating_times.append(time)
+                prev_size = current_size
 
-                # Record the eating times of the snake
-                current_size = len(self.snake.snake)
-                if current_size > prev_size:
-                    eating_times.append(time)
-                    prev_size = current_size
+            # Update the time
+            time += 1
 
-                # Update the time
-                time += 1
+        # Calculate score of agent
+        fitness = len(self.snake.snake) - 1
+        deaths = 1 if self.snake.game_over() else 0
+        steps = [eating_times[i] - eating_times[i - 1]
+                 for i in range(1, len(eating_times))]
 
-            # Calculate score of agent
-            fitness.append(len(self.snake.snake) - 1)
-
-            deaths += 1 if self.snake.game_over() else 0
-
-            temp_steps = [eating_times[i] - eating_times[i - 1]
-                          for i in range(1, len(eating_times))]
-            for step in temp_steps:
-                steps.append(step)
-
-            penalties += penalty
-
-            # Reset the snake
-            self.snake.reset()
+        # Reset the snake
+        self.snake.reset()
 
         # Calculate and update the agents fitness
-        self.fitness = max(fitness) * 5
-        self.fitness = self.fitness - deaths * 0.5 - penalties * 1
+        self.fitness = fitness * 5
+        self.fitness = self.fitness - deaths * 0.5 - penalty * 1
         self.fitness = self.fitness - \
             mean(steps) * 0.1 if len(steps) > 0 else self.fitness
