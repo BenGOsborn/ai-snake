edb7deca5cfdca44f6a50ca809833a6e5b0b7c8f
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-21 13:29:19 +1100

    🎉 Update

--- utils.py
+++ utils.py
@@ -12,6 +12,6 @@ GENERATION_SIZE = 100
 TOP_K = 25
 MUTATION_CHANCE = 0.15
 MUTATE_POP_CHANCE = 0.20
-GENERATIONS = 10000
+GENERATIONS = 200
 
 MODEL_PATH_GA = os.path.join(os.getcwd(), "bin", "model_ga.pth")
