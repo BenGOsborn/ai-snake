f590d5bbf0363bd6d1c81dbc5869c155c7ea41a8
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-22 08:20:25 +1100

    🎉 Update

---
+++ model/dqn/model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+
+class DQNModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.network = nn.Sequential(
+            nn.Linear(20, 16),
+            nn.ReLU(),
+            nn.Linear(16, 4),
+        )
+
+    def forward(self, inputs):
+        return self.network(inputs)
