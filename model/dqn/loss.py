02ae579b9c499777e0415957bba1a9594a0d918a
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-22 09:09:20 +1100

    🎉 Update

--- model/dqn/loss.py
+++ model/dqn/loss.py
@@ -1,3 +1,11 @@
+import torch.nn as nn
+
+
+mse_loss = nn.MSELoss()
+
+
 # Loss function
-def loss_fn():
-    pass
+def loss_fn(reward, q_max, gamma, prediction):
+    target = reward + gamma * q_max
+
+    return mse_loss(target, prediction)
