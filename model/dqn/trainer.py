04f9d93ae5f71d18e0f6646a05673443556407b4
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-22 08:35:10 +1100

    🎉 Update

--- model/dqn/trainer.py
+++ model/dqn/trainer.py
@@ -3,5 +3,13 @@ from model.agent import Agent
 
 
 class DQNTrainer:
-    def __init__(self):
+    def __init__(self, epsilon=0.9, epsilon_dec=0.996, epsilon_end=0.01, time_limit=1000):
+        self.time_limit = time_limit
+
+        # Store experience tuples (s_t, a_t, r_t+1, s_t+1)
+        self.states = None
+        self.actions = None
+        self.rewards = None
+
+    def run_epoch(self):
         pass
