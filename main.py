4d468f1ea4aec4ed5eae98770ff0964eb04b1984
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-11-22 13:39:41 +1100

    🎉 Update

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def run_dqn(stdscr):
     display = Display(snake, stdscr)
 
     model = DQNModel()
-    model.load_state_dict(torch.load(utils.MODEL_PATH_GA))
+    model.load_state_dict(torch.load(utils.MODEL_PATH_DQN))
     model.eval()
 
     # Game loop
@@ -87,6 +87,32 @@ def run_dqn(stdscr):
         sleep(1 / utils.FRAME_RATE)
 
 
+def run_dqn_noscr():
+    snake = Snake(
+        utils.HEIGHT,
+        utils.WIDTH,
+        utils.FOOD_AMOUNT,
+        seed=utils.SNAKE_SEED,
+    )
+
+    model = DQNModel()
+    model.load_state_dict(torch.load(utils.MODEL_PATH_DQN))
+    model.eval()
+
+    # Game loop
+    while True:
+        state = snake.get_state()
+        key, _ = choose_key(state, model)
+
+        print(
+            f"Snake size {len(snake.snake)} - Snake head {snake.snake[0]} - Food {snake.food}\nState - {state}"
+        )
+
+        snake.update_state(key)
+
+        sleep(1 / utils.FRAME_RATE)
+
+
 if __name__ == "__main__":
     if "ga" in sys.argv:
         if "d" in sys.argv:
@@ -94,6 +120,9 @@ if __name__ == "__main__":
         else:
             run_ga_noscr()
     elif "dqn" in sys.argv:
-        pass
+        if "d" in sys.argv:
+            curses.wrapper(run_dqn)
+        else:
+            run_dqn_noscr()
     else:
         raise Exception("Invalid arguments")
